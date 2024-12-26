89883f254ced84ede4908034d24c565842996179
Gabriel Carvalho <gacarvalho.contato@gmail.com>
2024-12-26 03:05:04 -0300

    ajuste cols e import

--- src/repo_agg_all_apps_gold.py
+++ src/repo_agg_all_apps_gold.py
@@ -14,7 +14,7 @@ except ModuleNotFoundError:
 
 
 # Configuração de logging
-logging.basicConfig(level=logging.INFO, start_collectionformat='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
     try:
